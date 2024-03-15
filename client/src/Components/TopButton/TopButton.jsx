c0529b71a7350fdeea2092861114fcaee700a231
Anson892 <1155175080@link.cuhk.edu.hk>
2024-03-15 18:07:34 +0800

    Improve scroll to top button UI

--- client/src/Components/TopButton/TopButton.jsx
+++ client/src/Components/TopButton/TopButton.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import "./TopButton.css"
+import scroll_to_top_btn from '../../Assets/UI/scroll_to_top_btn.svg'
 
-const top = () => {
+const handleTopOnClick = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
 export const TopButton = () => {
     return(
-    <div>
-    <button onClick={top} className='topbutton'> 
-        <p>Top</p>
-    </button>
+    <div className='top-btn'>
+        <img onClick={handleTopOnClick} src={ scroll_to_top_btn } alt="" />
     </div>
     )
 };
