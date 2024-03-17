c68f40cbb3093b9362ec1a2485f6663e9b6770ef
Anson892 <1155175080@link.cuhk.edu.hk>
2024-03-17 17:00:01 +0800

    Fix top button error due to smooth scroll

--- client/src/Components/TopButton/TopButton.jsx
+++ client/src/Components/TopButton/TopButton.jsx
@@ -3,8 +3,12 @@ import "./TopButton.css"
 import scroll_to_top_btn from '../../Assets/UI/scroll_to_top_btn.svg'
 
 const handleTopOnClick = () => {
+    document.body.style.scrollBehavior = "smooth"
+    document.documentElement.style.scrollBehavior = "smooth"
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+    document.body.style.scrollBehavior = "auto"
+    document.documentElement.style.scrollBehavior = "auto"
 }
 
 export const TopButton = () => {
