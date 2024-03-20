ba058f8151832582811d324568adea84cd1597a5
Anson892 <1155175080@link.cuhk.edu.hk>
2024-03-20 16:25:18 +0800

    Fix navbar position not fixed error due to page transition

--- client/src/Components/Animations/PageTransition/PageTransition.jsx
+++ client/src/Components/Animations/PageTransition/PageTransition.jsx
@@ -13,18 +13,21 @@ export const PageTransition = ({ children }) => {
                 await animate([
                     [".upper-overlay", {height: "0vh"}, {duration: 2, at: 0.1, ease: [0.16, 1, 0.3, 1]}],
                     [".lower-overlay", {height: "0vh"}, {duration: 2, at: "<", ease: [0.16, 1, 0.3, 1]}],
-                    [".black-filter", { filter: "brightness(1)"}, {duration: 1.5, at: 0, ease: [0.32, 0, 0.67, 0]}],
+                    [".black-filter", { opacity: 0}, {duration: 1.5, at: 0, ease: [0.32, 0, 0.67, 0]}],
+                    [".page-container", { opacity: 1}, {duration: 2, at: 0, ease: [0.32, 0, 0.67, 0]}],
                 ]);
+                document.getElementById("black-filter").style.display = "none";
             }
             enterAnimation();
         }
         else {
             const exitAnimation = async() => {
                 await animate([
+                    [".black-filter", { display: "block"}, {duration: 0, at: 0, ease: [0.32, 0, 0.67, 0]}],
                     [".upper-overlay", {height: "50vh"}, {duration: 1, at: 0, ease: [0.87, 0, 0.13, 1]}],
                     [".lower-overlay", {height: "50vh"}, {duration: 1, at: "<", ease: [0.87, 0, 0.13, 1]}],
-                    [".black-filter", { top: "55vh"}, {duration: 0.90, at: 0.1, ease: [0.32, 0, 0.67, 0]}],
-                    [".black-filter", { filter: "brightness(0.3)"}, {duration: 0.65, at: 0, ease: [0.32, 0, 0.67, 0]}],
+                    [".page-container", { top: "55vh"}, {duration: 0.90, at: 0.1, ease: [0.32, 0, 0.67, 0]}],
+                    [".black-filter", { opacity: 0.85}, {duration: 0.65, at: 0, ease: [0.32, 0, 0.67, 0]}],
                 ]);
                 safeToRemove();
             }
@@ -36,7 +39,8 @@ export const PageTransition = ({ children }) => {
         <div className="page-transition" ref={scope}>
             <div className="upper-overlay"/>
             <div className="lower-overlay"/>
-            <div className="black-filter">
+            <div className="black-filter" id="black-filter"></div>
+            <div className="page-container">
                 {children}
             </div>
         </div>
