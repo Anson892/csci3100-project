353c7842bb6564ab52f6e6d3afe8752d9382c2b6
paprika-dev <newtoov@Jasonde-MacBook-Air.local>
2024-03-22 15:04:39 +0800

    login and register page basic UI

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import loginImage from '../../Assets/Images/LoginImage.jpg'
+import { motion, useScroll, useMotionValueEvent, useTransform } from 'framer-motion'
+import './Login.css'
 
+
+const LoginForm = () => {
+  return (
+    <form class="loginForm">
+      <p class="title">Welcome back</p>
+      <div class="inputContainer">
+        <input type="text" required/>
+        <label>Username</label>
+      </div>
+      <div class="inputContainer">
+        <input type="text" required/>
+        <label>Password</label>
+      </div>
+      <div class="redirectLink">
+        Don’t have an account yet? &nbsp;
+        <Link to={'/register'} onClick={()=>{window.scrollTo({top: (0, 0), behavior: 'instant'})}}>Sign up</Link>
+      </div>
+      <Link to={'/'} onClick={()=>{window.scrollTo({top: (0, 0), behavior: 'instant'})}}>
+        <button class="loginButton" type="submit">Sign In</button>
+      </Link>
+    </form>
+    
+  )
+}
 export const Login = () => {
   return (
     <div>
-        Login
+      <LoginForm/>
+      <img class="loginImage" src={loginImage} alt='login img'></img>
     </div>
   )
 }
--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import loginImage from '../../Assets/Images/RegisterImage.jpg'
+import { motion, useScroll, useMotionValueEvent, useTransform } from 'framer-motion'
+import '../Login/Login.css'
 
+
+const LoginForm = () => {
+  return (
+    <form class="loginForm">
+      <p class="title">Create account</p>
+      <div class="inputContainer">
+        <input type="text" required/>
+        <label>Username</label>
+      </div>
+      <div class="inputContainer">
+        <input type="text" required/>
+        <label>Password</label>
+      </div>
+      <div class="redirectLink">
+        Already have an account? &nbsp;
+        <Link to={'/login'} onClick={()=>{window.scrollTo({top: (0, 0), behavior: 'instant'})}}>Sign in</Link>
+      </div>
+      <Link to={'/'} onClick={()=>{window.scrollTo({top: (0, 0), behavior: 'instant'})}}>
+        <button class="loginButton" type="submit">Sign Up</button>
+      </Link>
+    </form>
+    
+  )
+}
 export const Register = () => {
   return (
     <div>
-        Register
+      <LoginForm/>
+      <img class="loginImage" src={loginImage} alt='login img'></img>
     </div>
   )
 }
